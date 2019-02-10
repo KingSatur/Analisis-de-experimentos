838c3f3fd4c80107caf409aeb0c77069141f723e
DavidSatur <juancawool@outlook.com>
2019-02-10 13:02:36 -0500

    Algorithms with their respective times in nanoseconds according to the factor

--- Análisis de experimentos/Análisis de experimentos/Program.cs
+++ Análisis de experimentos/Análisis de experimentos/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,48 +13,23 @@ namespace Análisis_de_experimentos
         private TimeSpan start;
         private double time;
 
-        public int[] CountingSort(int[] A)
+        private int[] fillArray(int n)
         {
-            start = new TimeSpan(DateTime.Now.Ticks);
+            int[] a = new int[n];
 
-            int n = A.Length - 1;
-            int k = A[0];
-            for (int i = 1; i <= n; i++)
+            for (int i = 0; i < a.Length; ++i)
             {
-                if (A[i] > k)
+                if (i % 3 == 0)
                 {
-                    k = A[i];
+                    a[i] = -(new Random().Next(i * 200 + 1));
+                }
+                else
+                {
+                    a[i] = new Random().Next(i * 300 + 1);
                 }
             }
-            int[] C = new int[k + 1];
 
-            for (int i = 0; i <= k; i++)
-            {
-                C[i] = 0;
-            }
-
-            for (int i = 0; i <= n - 1; i++)
-            {
-                C[A[i]] = C[A[i]] + 1;
-            }
-
-            for (int i = 1; i <= k; i++)
-            {
-                C[i] = C[i] + C[i - 1];
-            }
-
-            int[] B = new int[n + 1];
-            for (int i = n; i >= 0; i--)
-            {
-                C[A[i]] = C[A[i]] - 1;
-                B[C[A[i]]] = A[i];
-            }
-
-            stop = new TimeSpan(DateTime.Now.Ticks);
-            time = stop.Subtract(start).TotalMilliseconds;
-
-            return B;
-
+            return a;
         }
 
         public void MergeSort(int[] input, int low, int high)
@@ -69,7 +45,13 @@ namespace Análisis_de_experimentos
 
         public void MergeSort(int[] input)
         {
+            start = new TimeSpan(DateTime.Now.Ticks);
+
             MergeSort(input, 0, input.Length - 1);
+
+            stop = new TimeSpan(DateTime.Now.Ticks);
+            time = stop.Subtract(start).TotalMilliseconds;
+
         }
 
         private void Merge(int[] input, int low, int middle, int high)
@@ -121,6 +103,97 @@ namespace Análisis_de_experimentos
             }
 
         }
+
+        public void generarRepeticiones()
+        {
+
+            for(int j = 0; j < 4; j++)
+            {
+                Stream fs = new FileStream("./MergeSortFactor" + (j + 1) + ".txt", FileMode.Create, FileAccess.Write);
+                StreamWriter writer = new StreamWriter(fs);
+
+                for(int i = 0; i < 50; i++)
+                {
+                    if((j + 1) == 1)
+                    {
+                        MergeSort(fillArray(10));
+                        writer.WriteLine(time * 1000000);
+                    }
+                    else if((j + 1) == 2)
+                    {
+                        MergeSort(fillArray(1000));
+                        writer.WriteLine(time * 1000000);
+                    }
+                    else if((j + 1) == 3)
+                    {
+                        MergeSort(fillArray(10000));
+                        writer.WriteLine(time * 1000000);
+                    }
+                    else if((j + 1)== 4)
+                    {
+                        MergeSort(fillArray(1000000));
+                        writer.WriteLine(time * 1000000);
+                    }
+
+                }
+
+                writer.Close();
+                fs.Close();
+            }
+
+
+
+            
+
+
+
+        }
+
+        public void generarRepeticion2()
+        {
+
+            for (int j = 0; j < 4; j++)
+            {
+                Stream fs = new FileStream("./CombSortFactor" + (j + 1) + ".txt", FileMode.Create, FileAccess.Write);
+                StreamWriter writer = new StreamWriter(fs);
+
+                for (int i = 0; i < 50; i++)
+                {
+                    if ((j + 1) == 1)
+                    {
+                        CombSort(fillArray(10));
+                        writer.WriteLine(time * 1000000);
+                    }
+                    else if ((j + 1) == 2)
+                    {
+                        CombSort(fillArray(1000));
+                        writer.WriteLine(time * 1000000);
+                    }
+                    else if ((j + 1) == 3)
+                    {
+                        CombSort(fillArray(10000));
+                        writer.WriteLine(time * 1000000);
+                    }
+                    else if ((j + 1) == 4)
+                    {
+                        CombSort(fillArray(1000000));
+                        writer.WriteLine(time * 1000000);
+                    }
+
+                }
+
+                writer.Close();
+                fs.Close();
+            }
+
+
+
+
+
+
+
+        }
+
 
 
         private int getNextGap(int gap)
@@ -178,30 +251,8 @@ namespace Análisis_de_experimentos
         public static void Main(string[] args)
         {
 
-            Ordenamientos algoritmos = new Ordenamientos();
-           
-            int[] a = new int[100];
-
-            for (int i = 0; i < a.Length; ++i)
-            {
-                if(i % 3 == 0)
-                {
-                    a[i] =-(new Random().Next(i * 200 + 1));
-                }
-                else
-                {
-                    a[i] = new Random().Next(i * 300 + 1);
-                }
-               
-            
-            }
-
-            algoritmos.MergeSort(a);
-        
-            for(int i =0; i < a.Length; i++)
-            {
-                Console.WriteLine(a[i]);
-            }
+            Ordenamientos m = new Ordenamientos();
+            m.generarRepeticion2();
            
 
 
