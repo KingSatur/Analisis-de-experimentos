64e03044097cf68eb00622145161c7681fcffb8a
DavidSatur <juancawool@outlook.com>
2019-02-10 22:55:32 -0500

    Unitary test updated

--- Análisis de experimentos/Unitary Test CombSort/TestMergeSort.cs
+++ Análisis de experimentos/Unitary Test CombSort/TestMergeSort.cs
@@ -25,41 +25,10 @@ namespace Unitary_Test_CombSort
             }
         }
 
+
         [TestMethod]
         public void TestFactorOne()
         {
-
-            ordenamientos = new Ordenamientos();
-            int[] arrayOne = { 10, 40, 100, 2, 39, 21, 23, 43, 69, 88 };
-            ordenamientos.MergeSort(arrayOne);
-            int[] toTry = { 2, 10, 21, 23, 39, 40, 43, 69, 88, 100 };
-
-            for (int i = 0; i < arrayOne.Length; i++)
-            {
-                Assert.IsTrue(arrayOne[i] == toTry[i]);
-            }
-
-        }
-
-        [TestMethod]
-        public void TestFactorTwo()
-        {
-            ordenamientos = new Ordenamientos();
-            int[] arrayOne = new int[100];
-            fillArray(arrayOne);
-            ordenamientos.MergeSort(arrayOne);
-
-            for (int i = 0; i < arrayOne.Length - 1; i++)
-            {
-                Assert.IsTrue((arrayOne[i] - arrayOne[i + 1]) <= 0);
-            }
-
-        }
-
-        [TestMethod]
-        public void TestFactorThree()
-        {
-
             ordenamientos = new Ordenamientos();
             int[] arrayOne = new int[10000];
             fillArray(arrayOne);
@@ -69,11 +38,27 @@ namespace Unitary_Test_CombSort
             {
                 Assert.IsTrue((arrayOne[i] - arrayOne[i + 1]) <= 0);
             }
+
+        }
+
+        [TestMethod]
+        public void TestFactorTwo()
+        {
+
+            ordenamientos = new Ordenamientos();
+            int[] arrayOne = new int[100000];
+            fillArray(arrayOne);
+            ordenamientos.MergeSort(arrayOne);
+
+            for (int i = 0; i < arrayOne.Length - 1; i++)
+            {
+                Assert.IsTrue((arrayOne[i] - arrayOne[i + 1]) <= 0);
+            }
         }
 
 
         [TestMethod]
-        public void TestFactorFour()
+        public void TestFactorThree()
         {
 
             ordenamientos = new Ordenamientos();
